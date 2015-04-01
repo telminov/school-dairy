2e998049207f30cf7f343d48452f21f546039585
Telminov Sergey <telminov@soft-way.biz>
2015-04-01 22:20:03 +0300

    поддержка базовой рест аутентификации

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import djutils.views.rest_auth
 
 urlpatterns = [
     url(r'^', include('dairy.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api/auth/login/$', djutils.views.rest_auth.login),
+    url(r'^api/auth/logout/$', djutils.views.rest_auth.logout),
+    url(r'^api/auth/current_user/$', djutils.views.rest_auth.get_current_user),
 ]
